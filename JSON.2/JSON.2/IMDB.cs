4f15f3ce777904c404a12aaa364640ce26a15249
Griffin Sink <Griffin.D.Sink@ou.edu>
2019-05-09 21:47:00 -0500

    yo yo yo

---
+++ JSON.2/JSON.2/IMDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON._2
+{
+    class IMDB
+    {
+      public string movie_Id { get; set; }
+      public string director_name               {get; set;}
+      public double num_critic_for_reviews      {get; set;}
+      public double duration                    {get; set;}
+      public double gross                       {get; set;}
+      public string genres                      {get; set;}
+      public string actor_1_name                {get; set;}
+      public string movie_title                 {get; set;}
+      public double num_voted_users             {get; set;}
+      public double cast_total_facebook_likes   {get; set;}
+      public string plot_keywords               {get; set;}
+      public string movie_imdb_link             {get; set;}
+      public double num_user_for_reviews        {get; set;}
+      public string language                    {get; set;}
+      public string country                     {get; set;}
+      public string content_rating              {get; set;}
+      public double budget                      {get; set;}
+      public double title_year                  {get; set;}
+      public double imdb_score                  {get; set;}
+      public double movie_facebook_likes        {get; set;}
+    }
+}
